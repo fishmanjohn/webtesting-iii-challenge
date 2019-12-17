5eac236d7267bf177b56cfc6460961e17eb4e5ca
fishmanjohn <john@emmaandjohnbyvan.com>
2019-12-16 19:02:51 -0800

    tests working I dont know how but they are

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Controls from './Controls'
+
+test('controlls renders without crashing', ()=>{
+    render(<Controls/>)
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -15,3 +15,21 @@ test('unlocked displays when gate is unlocked', () => {
     }
 });
 
+test('open displays when gate is open', ()=>{
+    const gate = render(<Dashboard/>)
+    if(gate.getByText(/Close Gate/i)){
+        gate.getByText(/Open/i)
+    }
+})
+
+test('lock button changes on click', ()=>{
+    const {getByTestId} = render(<Dashboard/>)
+    //const button = getByTestId(/buttonOne/i)
+    fireEvent.click(getByTestId(/buttonOne/i))
+    
+   })
+
+   test('Gate open changes on click', ()=>{
+       const {getByTestId} = render(<Dashboard/>)
+       fireEvent.click(getByTestId(/buttonTwo/i))
+   })
